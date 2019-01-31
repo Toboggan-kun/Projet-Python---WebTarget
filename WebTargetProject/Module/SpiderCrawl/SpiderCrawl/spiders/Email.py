b443b63003954cb8eacce9d9fa2b7ad6bdb8f789
Toboggan-kun <carolinetangsong@gmail.com>
2019-02-01 00:14:02 +0100

    EMAIL OK

---
+++ WebTargetProject/Module/SpiderCrawl/SpiderCrawl/spiders/Email.py
@@ -0,0 +1,8 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+class Email():
+    # Liste des e-mails a envoyer
+    recipients = ['cmkhmer@gmail.com', 'chalana.meng@gmail.com']
+
